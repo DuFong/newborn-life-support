87afab5634794a8164e01d9caa839231385bd1cb
hyeonQyu <dhk0561@naver.com>
2019-10-02 20:12:06 +0900

    MainActivity skeleton code, skeleton xml

--- app/src/main/java/com/example/nls/MainActivity.java
+++ app/src/main/java/com/example/nls/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.nls;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnGte100, btnLt100, btnLt60;
+    TextView txtTitle, txtStatus, txtTimer;
+    TextView txtChogi, txtYangap, txtMrsopa, txtGigwan, txtHeart, txtEpinephrine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGte100 = findViewById(R.id.gte_100);
+        btnLt100 = findViewById(R.id.lt_100);
+        btnLt60 = findViewById(R.id.lt_60);
+
+        txtStatus = findViewById(R.id.status);
+        txtTimer = findViewById(R.id.timer);
+
+        txtChogi = findViewById(R.id.chogi);
+        txtYangap = findViewById(R.id.yangap);
+        txtMrsopa = findViewById(R.id.mrsopa);
+        txtGigwan = findViewById(R.id.gigwan);
+        txtHeart = findViewById(R.id.heart);
+        txtEpinephrine = findViewById(R.id.epinephrine);
+
+        btnGte100.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 응급처치 종료
+            }
+        });
+
+        btnLt100.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 기본처치 -> 양압환기 -> MRSOPA -> 기관삽관 (유지)
+            }
+        });
+
+        btnLt60.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 기관삽관 -> 심장마사지, 에피네프린 투여
+            }
+        });
     }
 }
